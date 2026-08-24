00234da4b20330d0f67fb09e1da05a8f29a5baae
Emi.autism98 <Emi.Autism98@gmail.com>
2026-08-24 21:46:38 +0100

    Refactor scrape.py to improve structure and add TNWM
    Removed the parts where I thought I was funny
    
    Signed-off-by: Emi.autism98 <Emi.Autism98@gmail.com>

--- scrape.py
+++ scrape.py
@@ -1,51 +1,29 @@
-# A script to scrape the amount of routes and vehicles because I can't be arsed doing it myself
-print("Behold, the one, the only scrape script-a-tron-o-matic 5000 (website will likely be broken - holding your breath, crossing your fingers and touching wood recommended)")
-
-# Import libraries (duh)
 import requests
 from bs4 import BeautifulSoup
 import json
 
-# Routes and all that jazz
-print("Part 1: Scraping routes (if the website doesn't break again, which is a big if)")
 urlTUKG = "https://www.mybustimes.cc/group/Travel%20UK%20Group/"
 urlTNW = "https://www.mybustimes.cc/operator/travel-north-west/"
 urlTUKC = "https://www.mybustimes.cc/operator/travel-uk-coaches/"
 urlTNWR = "https://www.mybustimes.cc/operator/travel-north-west-rail/"
+urlTNWM = "https://www.mybustimes.cc/operator/merseytravel-metro-travel-north-west/"
 
 routesTUKG = None
 routesTNW = None
 routesTUKC = None
 routesTNWR = None
+routesTNWM = None
 vehiclesTUKG = None
 vehiclesTNW = None
 vehiclesTUKC = None
 vehiclesTNWR = None
+vehiclesTNWM = None
 
 responseTUKG = requests.get(urlTUKG)
 responseTNW = requests.get(urlTNW)
 responseTUKC = requests.get(urlTUKC)
 responseTNWR = requests.get(urlTNWR)
-
-if responseTUKG.status_code == 200:
-    print("Travel UK Group response (both pages): ", responseTUKG.status_code, "(that's good)")
-elif responseTUKG.status_code != 200:
-    print("Travel UK Group response (both pages): ", responseTUKG.status_code, "(that's not good - MBT has probably shat itself again - as booked)")
-
-if responseTNW.status_code == 200:
-    print("Travel North West response (routes): ", responseTNW.status_code, "(that's good)")
-elif responseTNW.status_code != 200:
-    print("Travel North West response (routes): ", responseTNW.status_code, "(that's not good - MBT has probably shat itself again - as booked)")
-
-if responseTUKC.status_code == 200:
-    print("Travel UK Coaches response (routes): ", responseTUKC.status_code, "(that's good)")
-elif responseTUKC.status_code != 200:
-    print("Travel UK Coaches response (routes): ", responseTUKC.status_code, "(that's not good - MBT has probably shat itself again - as booked)")
-
-if responseTNWR.status_code == 200:
-    print("Travel North West Rail response (routes): ", responseTNWR.status_code, "(that's good)")
-elif responseTNWR.status_code != 200:
-    print("Travel North West Rail response (routes): ", responseTNWR.status_code, "(that's not good - MBT has probably shat itself again - as booked)")
+responseTNWM = requests.get(urlTNWM)
 
 soupTUKG = BeautifulSoup(responseTUKG.text, "html.parser")
 itemsTUKG = soupTUKG.find_all("li")
@@ -77,16 +55,22 @@ for item in itemsTNWR:
     if "routes" in content:
         routesTNWR = int(content.split()[0].replace(",", ""))
 
-# Vehicles that use a separate page for whatever reason
-print("Part 2: Exactly the same but with vehicles/ at the end because why wouldn't it be? (You can probably breathe now)")
+soupTNWM = BeautifulSoup(responseTNWM.text, "html.parser")
+itemsTNWM = soupTNWM.find_all("li")
+for item in itemsTNWM:
+    content = item.get_text()
+    if "routes" in content:
+        routesTNWM = int(content.split()[0].replace(",", ""))
 
 vehUrlTNW = "https://www.mybustimes.cc/operator/travel-north-west/vehicles/"
 vehUrlTUKC = "https://www.mybustimes.cc/operator/travel-uk-coaches/vehicles/"
 vehUrlTNWR = "https://www.mybustimes.cc/operator/travel-north-west-rail/vehicles/"
+vehUrlTNWM = "https://www.mybustimes.cc/operator/merseytravel-metro-travel-north-west/vehicles/"
 
 vehResponseTNW = requests.get(vehUrlTNW)
 vehResponseTUKC = requests.get(vehUrlTUKC)
 vehResponseTNWR = requests.get(vehUrlTNWR)
+vehResponseTNWM = requests.get(vehUrlTNWM)
 
 soupVehTNW = BeautifulSoup(vehResponseTNW.text, "html.parser")
 itemsVehTNW = soupVehTNW.find_all("li")
@@ -109,7 +93,16 @@ for item in itemsVehTNWR:
     if "vehicles" in content:
         vehiclesTNWR = int(content.split()[0].replace(",", ""))
 
-# Last but not least, save all the blah-dee-blah to the whatchamacallit, and we (should) be good to go
+soupVehTNWM = BeautifulSoup(vehResponseTNWM.text, "html.parser")
+itemsVehTNWM = soupVehTNWM.find_all("li")
+for item in itemsVehTNWM:
+    content = item.get_text()
+    if "vehicles" in content:
+        vehiclesTNWM = int(content.split()[0].replace(",", ""))
+
+routesTNW = routesTNW + routesTNWM
+vehiclesTNW = vehiclesTNW + vehiclesTNWM
+
 data = {
     "routes_TUKG": routesTUKG,
     "routes_TNW": routesTNW,
@@ -124,8 +117,3 @@ data = {
 
 with open("stats.json", "w") as f:
     json.dump(data, f)
-
-print("Stats (hopefully) updated successfully")
-
-# That was way too much effort but now it's automated so that's a win
-# 5 hours once > 5 minutes every day. Sound logic
